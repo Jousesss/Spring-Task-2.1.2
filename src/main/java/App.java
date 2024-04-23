bb131bf4dd79511a3f054ad49673a05757e0109e
Aleksey Boorilov <aleksei.burilov@gmail.com>
2024-04-24 02:34:03 +0500

    finished task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld firstHW =
+                (HelloWorld) applicationContext.getBean("helloWorld");
+        System.out.println(firstHW.getMessage());
+        System.out.println("\n");
+
+        // Создание объектов
+        HelloWorld secondHW = applicationContext.getBean("helloWorld",HelloWorld.class);
+
+        Cat firstCat = applicationContext.getBean("cat",Cat.class);
+        Cat secondCat = applicationContext.getBean("cat",Cat.class);
+
+        System.out.println("firstHW == secondHW   (" + (firstHW == secondHW) + ")");
+        System.out.println("firstCat == secondCat   (" + (firstCat == secondCat) + ")");
     }
 }
